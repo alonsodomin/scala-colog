b6a32c90646bec5d759e19169e4a9f8b0d4e7405
Antonio Alonso Dominguez <antoniodominguez@rentalcars.com>
2019-01-10 11:43:55 +0000

    Align constraint on F with other loggers

--- slf4j/src/main/scala/colog/slf4j/Slf4jLoggers.scala
+++ slf4j/src/main/scala/colog/slf4j/Slf4jLoggers.scala
@@ -1,30 +1,32 @@
 package colog.slf4j
 
-import cats.effect.{IO, LiftIO}
+import cats.effect.Sync
+
 import colog.{LogRecord, Logger, Severity}
+
 import org.slf4j.{LoggerFactory, Logger => JLogger}
 
 object Slf4jLoggers {
 
-  def apply[F[_]](baseLogger: JLogger)(implicit F: LiftIO[F]): Logger[F, LogRecord] = {
+  def apply[F[_]](baseLogger: JLogger)(implicit F: Sync[F]): Logger[F, LogRecord] = {
     def unsafeLog(record: LogRecord)(f: (String, Throwable) => Unit): F[Unit] =
-      F.liftIO(IO(f(record.message, record.error.orNull)))
+      F.delay(f(record.message, record.error.orNull))
 
     Logger { record =>
-      val loggingFun: (String, Throwable) => Unit = record.severity match {
+      val logFn: (String, Throwable) => Unit = record.severity match {
         case Severity.Debug => baseLogger.debug
         case Severity.Info => baseLogger.info
         case Severity.Warning => baseLogger.warn
         case Severity.Error => baseLogger.error
       }
-      unsafeLog(record)(loggingFun)
+      unsafeLog(record)(logFn)
     }
   }
 
-  def forName[F[_]: LiftIO](name: String): Logger[F, LogRecord] =
+  def forName[F[_]: Sync](name: String): Logger[F, LogRecord] =
     apply[F](LoggerFactory.getLogger(name))
 
-  def forClass[F[_]: LiftIO](clazz: Class[_]): Logger[F, LogRecord] =
+  def forClass[F[_]: Sync](clazz: Class[_]): Logger[F, LogRecord] =
     apply[F](LoggerFactory.getLogger(clazz))
 
 }
