a4d158fd8759420cf93bb6806a499d0dc9ae7929
Scala Steward <me@scala-steward.org>
2019-06-13 01:54:04 +0200

    Update scala-java-time to 2.0.0-RC3

--- project/Versions.scala
+++ project/Versions.scala
@@ -17,7 +17,7 @@ object Versions {
   val slf4j     = "1.7.26"
   val scalaTest = "3.0.8"
   val scalamock = "4.2.0"
-  val scalaTime = "2.0.0-RC2"
+  val scalaTime = "2.0.0-RC3"
 
   object scalajs {
     val dom = "0.9.7"
